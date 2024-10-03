688d31b0222de02fae0a99c77d64a1b419f04ec5
Olexandr Fetisov <149307126+fetis-off@users.noreply.github.com>
2024-10-03 22:26:01 +0300

    update application.properties

--- target/generated-sources/annotations/org/project/springweb/mapper/impl/UserMapperImpl.java
+++ target/generated-sources/annotations/org/project/springweb/mapper/impl/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-03T15:55:14+0300",
+    date = "2024-10-03T22:25:18+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 @Component
